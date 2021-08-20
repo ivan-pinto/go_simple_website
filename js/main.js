625f57711a1a5bf88b4c6238b0374229f5e9e643
ivan-pinto <rubenivanpinto99@gmail.com>
2021-08-20 15:00:40 +0100

    Add text tp pt version.

--- js/main.js
+++ js/main.js
@@ -73,14 +73,14 @@
 /*                                                   */
 /*****************************************************/
 
-function showFormacao() {
-    document.getElementById('formacao').style.display = "block";
-    document.getElementById('experiencia').style.display = "none";
-}
-function showExperiencia() {
-    document.getElementById('experiencia').style.display = "block";
-    document.getElementById('formacao').style.display = "none";
- }
+    function showFormacao() {
+        document.getElementById('formacao').style.display = "block";
+        document.getElementById('experiencia').style.display = "none";
+    }
+    function showExperiencia() {
+        document.getElementById('experiencia').style.display = "block";
+        document.getElementById('formacao').style.display = "none";
+    }
 
 /*****************************************************/
 /*                                                   */
@@ -88,22 +88,30 @@ function showExperiencia() {
 /*                                                   */
 /*****************************************************/
 
-//Get the button:
-mybutton = document.getElementById("myBtn");
+    //Get the button:
+    var mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
+    function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+    }
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+    // When the user clicks on the button, scroll to the top of the document
+    function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    }
+
+/*****************************************************/
+/*                                                   */
+/*   Get Repos                                       */
+/*                                                   */
+/*****************************************************/
+
+  
